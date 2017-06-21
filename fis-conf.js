94418b14cd190d985d247cd5a9b6a6ca1a4a50c0
huyang <aaahy2011@sina.com>
2017-06-21 17:35:38 +0800

    20170621

--- fis-conf.js
+++ fis-conf.js
@@ -6,17 +6,26 @@
 //   useHash: false
 // });
 
-// fis.match('*.js', {
-//   optimizer: fis.plugin('uglify-js')
-// });
+/*fis.match('*.js', {
+  optimizer: fis.plugin('uglify-js')
+});*/
 
 // fis.match('*.css', {
 //   useSprite: true,
 //   optimizer: fis.plugin('clean-css')
 // });
 
+//把/Template/default下的.scss全部变为.css
+fis.match('/Template/default/**/*.scss', {
+  // fis3-parser-scss 插件进行解析
+  parser: fis.plugin('scss'),
+  // .scss 文件后缀构建后被改成 .css 文件
+  rExt: '.css'
+})
+
 // fis.match('*.png', {
 //   optimizer: fis.plugin('png-compressor')
+// });
 // });
 
 
@@ -27,7 +36,7 @@ fis.media('qa').match('*', {
     to: '/var/www/html/hyblog' // 注意这个是指的是测试机器的路径，而非本地机器
   })
 });
-fis.match('*', {
+fis.media('qa').match('*', {
     //访问url是/hyblog/xxx
     url : '/hylog$0'
 });
